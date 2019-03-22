a59c0369331e8508f65fbdaba133bed982f3b5e4
Christoph Rueff <christoph.rueff@gmail.com>
2019-03-22 16:05:43 +0100

    ch.9 p.42 #22

--- src/Money.java
+++ src/Money.java
@@ -7,7 +7,7 @@ abstract class Money {
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, null);
     }
 
     abstract Money times(int multiplier);

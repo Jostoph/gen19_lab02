8caaa9ce721f73038b69701e412abda890d406f3
Christoph Rueff <christoph.rueff@gmail.com>
2019-03-22 15:26:06 +0100

    ch.8 p.38 #16

--- src/Money.java
+++ src/Money.java
@@ -1,9 +1,11 @@
-public class Money {
+abstract class Money {
     protected int amount;
 
     static Dollar dollar(int amount) {
         return new Dollar(amount);
     }
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object object) {

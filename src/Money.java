a3d5bfae420659671d441d57dc22c24f27458d49
Christoph Rueff <christoph.rueff@gmail.com>
2019-03-22 16:56:45 +0100

    ch.10 p.48 Fixes #26

--- src/Money.java
+++ src/Money.java
@@ -15,8 +15,8 @@ class Money {
         return new Franc(amount, "CHF");
     }
 
-    Money times(int amount) {
-        return null;
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
     }
 
     String currency() {

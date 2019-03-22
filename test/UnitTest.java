6cd52f9f1e64a5e36718955a8227db0c3e1ba831
Christoph Rueff <christoph.rueff@gmail.com>
2019-03-22 16:52:31 +0100

    ch.10 p.47.1 #26

--- test/UnitTest.java
+++ test/UnitTest.java
@@ -27,8 +27,14 @@ public class UnitTest {
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
+    @Test
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
+    }
+
+    @Test
+    public void testDifferentClassEquality() {
+        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }

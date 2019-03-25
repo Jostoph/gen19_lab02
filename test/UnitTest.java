07a9f94a7784de7b93b757fa6218307b38203344
Christoph Rueff <christoph.rueff@gmail.com>
2019-03-25 13:36:38 +0100

    ch.12 p.58 #31

--- test/UnitTest.java
+++ test/UnitTest.java
@@ -57,4 +57,11 @@ public class UnitTest {
         assertEquals(Money.dollar(7), result);
     }
 
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
 }

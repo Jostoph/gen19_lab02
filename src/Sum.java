5761c33423b1046cdc66f753a4c780f6b7ea60b8
Christoph Rueff <christoph.rueff@gmail.com>
2019-03-25 16:46:57 +0100

    ch.15 p.69 Fixes #37, Fixes #38, Fixes #39

--- src/Sum.java
+++ src/Sum.java
@@ -8,6 +8,10 @@ public class Sum implements Expression {
         this.addend = addend;
     }
 
+    public Expression times(int multiplier) {
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
+    }
+
     public Money reduce(Bank bank, String to) {
         int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
         return new Money(amount, to);
